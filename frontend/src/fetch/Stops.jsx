a7140404e315aebed6d3309942f46edf35bb6287
mlo20030 <74330662+mlo20030@users.noreply.github.com>
2023-12-07 11:07:51 -0500

    feat: added stop marker and info when clicked

--- frontend/src/fetch/Stops.jsx
+++ frontend/src/fetch/Stops.jsx
@@ -3,3 +3,7 @@ import { fetchURL } from './Fetch.jsx';
 export function getAllStops() {
     return fetchURL('stops');
 }
+
+export function getShuttlesByStopID(stopId) {
+    return fetchURL(`stops?stopID=${stopId}`);
+}
